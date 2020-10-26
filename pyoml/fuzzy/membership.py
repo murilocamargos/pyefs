0b25e9faf56857c217ccb72cd7cac8ef0b102274
Murilo Camargos <murilo.camargosf@gmail.com>
2020-10-26 14:50:01 -0300

    Create first test case for a tirangular MF

---
+++ pyoml/fuzzy/membership.py
@@ -0,0 +1,10 @@
+# Authors: Murilo Camargos <murilo.camargosf@gmail.com>
+# License: TBD
+
+__all__ = [
+    'TriMF',
+]
+
+class TriMF:
+    def __init__(self):
+        pass
---
+++ pyoml/fuzzy/tests/test_membership.py
@@ -0,0 +1,6 @@
+from ..membership import TriMF
+
+def test_trimf_set_abc_params():
+    # Test getting/setting the parameters of the Triangular MF
+    mf = TriMF(2,1,0)
+    assert mf.get_params() == (0,1,2)
