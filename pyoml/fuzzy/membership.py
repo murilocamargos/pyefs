1678c7bfac437a57ca7121d07c4a5bba23df3dc2
Murilo Camargos <murilo.camargosf@gmail.com>
2020-10-26 15:01:46 -0300

    Add triangular MF code

--- pyoml/fuzzy/membership.py
+++ pyoml/fuzzy/membership.py
@@ -6,5 +6,48 @@ __all__ = [
 ]
 
 class TriMF:
-    def __init__(self):
-        pass
+    """Triangular membership function.
+
+    Parameters
+    ----------
+    p1 : float
+        First parameter to control the triangular shape.
+
+    p2 : float
+        Second parameter to control the triangular shape.
+
+    p3 : float
+        Third parameter to control the triangular shape.
+
+    Attributes
+    ----------
+    a_ : float
+        Triangular shape lower bound.
+
+    b_ : float
+        Triangular shape mid point.
+
+    c_ : float
+        Triangular shape upper bound.
+    
+    Examples
+    --------
+    >>> from pyoml.fuzzy.membership import TriMF
+    >>> mf = TriMF(0,1,2)
+    """
+    def __init__(self, p1: float, p2: float, p3: float):
+        p = sorted([p1, p2, p3])
+        self.a_ = p[0]
+        self.b_ = p[1]
+        self.c_ = p[2]
+
+    def get_params(self):
+        """Get the triangular form's parameters.
+
+        Returns
+        -------
+        params : tuple(float, float, float)
+            Ordered triangular three parameters (a,b,c).
+        """
+        params = (self.a_, self.b_, self.c_)
+        return params
