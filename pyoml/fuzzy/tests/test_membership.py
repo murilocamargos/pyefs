96f95eac4014ff545af0c60feb38ede53736660a
Murilo Camargos <murilo.camargosf@gmail.com>
2020-10-27 19:29:09 -0300

    Create test to check if params are numeric

--- pyoml/fuzzy/tests/test_membership.py
+++ pyoml/fuzzy/tests/test_membership.py
@@ -1,3 +1,4 @@
+import pytest
 from pyoml.fuzzy.membership import TriMF
 
 def test_trimf_set_abc_params():
@@ -5,8 +6,14 @@ def test_trimf_set_abc_params():
     mf = TriMF(2,1,0)
     assert mf.get_params() == (0,1,2)
 
+def test_trimf_set_non_numeric_abc_params():
+    # Test setting a non numeric value for the MF params
+    with pytest.raises(TypeError) as err:
+        mf = TriMF('0',1,2)
+    assert str(err.value) == 'All parameters must be numeric.'
+
 def test_trimf_get_membership_degree():
-    # Test getting/setting the parameters of the Triangular MF
+    # Test the membership degrees computation for the Triangular MF
     mf = TriMF(2,1,0)
     assert mf.get_degree(-0.5) == 0
     assert mf.get_degree(0) == 0
