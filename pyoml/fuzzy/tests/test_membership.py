c014930d3c309fbad1a15d66b3856a44fa24b91a
Murilo Camargos <murilo.camargosf@gmail.com>
2020-10-27 19:38:18 -0300

    Create test to check parameter order

--- pyoml/fuzzy/tests/test_membership.py
+++ pyoml/fuzzy/tests/test_membership.py
@@ -3,18 +3,23 @@ from pyoml.fuzzy.membership import TriMF
 
 def test_trimf_set_abc_params():
     # Test getting/setting the parameters of the Triangular MF
-    mf = TriMF(2,1,0)
+    with pytest.raises(ValueError) as err:
+        _ = TriMF(2,1,0)
+    assert str(err.value) == 'The parameters must be specified such that a <= b <= c.'
+    
+    # Ordered
+    mf = TriMF(0,1,2)
     assert mf.get_params() == (0,1,2)
 
 def test_trimf_set_non_numeric_abc_params():
     # Test setting a non numeric value for the MF params
     with pytest.raises(TypeError) as err:
-        mf = TriMF('0',1,2)
+        _ = TriMF('0',1,2)
     assert str(err.value) == 'All parameters must be numeric.'
 
 def test_trimf_get_membership_degree():
     # Test the membership degrees computation for the Triangular MF
-    mf = TriMF(2,1,0)
+    mf = TriMF(0,1,2)
     assert mf.get_degree(-0.5) == 0
     assert mf.get_degree(0) == 0
     assert mf.get_degree(0.5) == 0.5
