b91997831239b3f856a98053ea9a9172a1a47a3c
Murilo Camargos <murilo.camargosf@gmail.com>
2020-10-27 20:00:18 -0300

    Implement get_params for the TrapMF

--- pyoml/fuzzy/membership.py
+++ pyoml/fuzzy/membership.py
@@ -6,6 +6,7 @@ NumericType = Union[int, float]
 
 __all__ = [
     'TriMF',
+    'TrapMF',
 ]
 
 class MF:
@@ -47,30 +48,30 @@ class TrapMF(MF):
 
     Parameters
     ----------
-    a : float
+    a : NumericType
         First parameter to control the trapzoidal shape.
 
-    b : float
+    b : NumericType
         Second parameter to control the trapzoidal shape.
 
-    c : float
+    c : NumericType
         Third parameter to control the trapzoidal shape.
     
-    d : float
+    d : NumericType
         Fourth parameter to control the trapzoidal shape.
 
     Attributes
     ----------
-    a_ : float
+    a_ : NumericType
         Trapezoidal shape lower bound.
 
-    b_ : float
+    b_ : NumericType
         Trapezoidal shape first mid point.
 
-    c_ : float
+    c_ : NumericType
         Trapezoidal shape second mid point.
     
-    d_ : float
+    d_ : NumericType
         Trapezoidal shape upper bound.
     
     Examples
@@ -89,6 +90,17 @@ class TrapMF(MF):
         self.b_ = b
         self.c_ = c
         self.d_ = d
+    
+    def get_params(self) -> Tuple[NumericType]:
+        """Get the trapezoidal form's parameters.
+
+        Returns
+        -------
+        params : Tuple[NumericType, NumericType, NumericType, NumericType]
+            Ordered trapezoidal four parameters (a,b,c,d).
+        """
+        params = (self.a_, self.b_, self.c_, self.d_)
+        return params
 
 
 class TriMF(MF):
@@ -96,24 +108,24 @@ class TriMF(MF):
 
     Parameters
     ----------
-    a : float
+    a : NumericType
         First parameter to control the triangular shape.
 
-    b : float
+    b : NumericType
         Second parameter to control the triangular shape.
 
-    c : float
+    c : NumericType
         Third parameter to control the triangular shape.
 
     Attributes
     ----------
-    a_ : float
+    a_ : NumericType
         Triangular shape lower bound.
 
-    b_ : float
+    b_ : NumericType
         Triangular shape mid point.
 
-    c_ : float
+    c_ : NumericType
         Triangular shape upper bound.
     
     Examples
@@ -136,7 +148,7 @@ class TriMF(MF):
 
         Returns
         -------
-        params : tuple(float, float, float)
+        params : Tuple[NumericType, NumericType, NumericType]
             Ordered triangular three parameters (a,b,c).
         """
         params = (self.a_, self.b_, self.c_)
@@ -148,13 +160,13 @@ class TriMF(MF):
 
         Parameters
         ----------
-        x : float
+        x : NumericType
             Input data for which the membership degree will be
             computed.
         
         Returns
         -------
-        degree : float 
+        degree : NumericType 
             The membership degree for the input `x`. The value
             is in [0, 1].
         """
